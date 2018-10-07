85b74e16d98429f17681e11bcc72700220863cb6
nicoto <toulousenicholas@gmail.com>
2018-10-06 18:00:31 -0700

    initial refactor with arrays

--- assets/js/bibletrivia.js
+++ assets/js/bibletrivia.js
@@ -93,72 +93,71 @@ var revelation = {
 const oldTestament = { ...pentateuch, ...historicalBooks, ...wisdomBooks, ...propheticBooks };
 const newTestament = { ...theGospels, ...historicalBook, ...paulineEpistles, ...generalEpistles, ...revelation };
 // const bible = { ...oldTestament, ...newTestament };
+
 const bible = {'1 Peter': 'This book of the Bible is a General Epistle (Apostolic Letter). It was written to all believers in general. The author is Peter who wrote it about 60 A.D. The key personalities are the Apostle Peter, Silas, and Mark. Its purpose was to encourage suffering Christians and to call them to personal holiness; Peter’s central focus is persecution.',
 '1 Thessalonians': 'The book of 1st Thessalonians is a Pauline Epistle (letter from Paul). The Apostle Paul wrote it about 52-54 A.D. and it was one of his earliest written letters. The key personalities in this book are the Apostle Paul, Timothy, and Silas. Paul wrote this letter to strengthen and encourage the church in Thessalonica. To encourage and hearten the believers, Paul chose to emphasize the second coming of Jesus Christ. Throughout this letter, Paul focused on the principles of Faith, Hope, and Love.'}
-var bookNamesArray; // 73 at start
 
-var correct = 0;
-var answered = 0;
+var secretWord;
+var toCompare;
+var badGuesses;
+var bookNames = Object.keys(bible); // Get all 73 bible books in an array
+var remaining = bible.length;
+var wins, answered;
 
-var lttrsAlreadyGuessed = "";
-var numGuessesRemaining = 0;
-var badGuesses = [];
-
-var setUp = function () { // Immediately-invoked function expression
-    bookNamesArray = Object.keys(bible); // Get all 73 bible books in an array
-    alert("Click to begin the game");
-    document.addEventListener("click", function (event) {
-            updateScreen();
-    });
-    return;
-}();
+var displayVictory = () => {
+    var play = confirm("Congratulations! You answered " + wins + " of 73 books.")
+    if (play) {
+        initialize();
+    } else {
+        return;
+    }
+}
 
 var updateScreen = () => {
-    // pick a random word.
-    console.log("Enter a letter for a guess to match the following description.");
-
     // choose a book at random
-    if (bookNamesArray.length > 0) {
-        var i = Math.floor(Math.random() * bookNamesArray.length);
-        var book = bookNamesArray[i];
-        displayNewGuess(book);
+    if (remaining > 0) {
+        var i = Math.floor(Math.random() * bookNames.length);
+        var book = bookNames[i].toUpperCase();
+        // Display notecard prompt for selected book
+        document.getElementsByClassName("description")[0].innerHTML = bible[book];
+        displayHiddenWord(book);
     } else {
         displayVictory();
     }
     return;
 }
 
-var displayNewGuess = (book) => {
+var displayHiddenWord = (book) => {
 
-    document.getElementsByClassName("description")[0].innerHTML = bible[book];
+    console.log("Secret word looks like this: ", secretWord.join(" "));
 
-    // fill in underscores for hidden book of the Bible
-    var guessStr = "";
-
-    numGuessesRemaining = book.length;
-    for (var i = 0; i < book.length; i++) {
-        (book[i] === " ") ? guessStr += "\t" : guessStr += "_ ";
+    // fill in underscores for blank letters of the hidden book of the Bible
+    for(var i=0; i < book.length; i++) {
+        (book[i] === " ") ? secretWord.push("<span class='tab char'>&#09;</span>") : secretWord.push("<span class='char'>___</span>");
     }
-    console.log("guess string ", guessStr);
-    //document.getElementsByClassName("word")[0].innerHTML = guessStr;
-    processGuess(book, guessStr);
+
+    // show secret word on page
+    document.getElementsByClassName("word")[0].innerHTML = secretWord.join(" ");
+    processGuess(book);
     return;
 }
 
-var processGuess = (book, guessStr) => {
+var processGuess = (book) => {
 
-    // book name copy to record eliminating characters typed by user
-    var bk = book.slice(0, book.length).toUpperCase();
-    // set total guesses of book
-    numGuessesRemaining = book.length;
+    for (var i = 0; i < book.length; i++) { toCompare.push(book[i]); }
+    console.log(toCompare.join(""));
 
     // wait for user to hit a key
     document.addEventListener("keyup", function (event) {
-        // 1 Corinthians
+
+        // if they get it right
+        //remaining -= 1;
 
         // Derive & capitalize letter from key code
         var keychar = String.fromCharCode(event.keyCode).toUpperCase(); 
+       console.log("raw key ", event.key, " processed key char ", keychar);
        
+       return;
         // If letter has not already been guessed and is contained in the book name
         if ( bk.includes(keychar) && !lttrsAlreadyGuessed.includes(keychar) ) {
             // loop through the book, search and replace any underscores of hidden book name with key character
@@ -187,7 +186,7 @@ var processGuess = (book, guessStr) => {
     });
     console.log("this is it ", guessStr); //    _ 	_ _ _ _ _ _ _ _ _ _ _ _ _ for 1 Thessalonians
     document.getElementsByClassName("word")[0].innerHTML = guessStr;
-    return;
+    //return;
 }
 
 var nextTurn = () => {
@@ -204,3 +203,91 @@ var displayVictory = () => {
     alert("Congratulations, you have successfully reviewed all the books of the Bible!")
     return;
 }
+
+var initialize = function () { // Immediately-invoked function expression
+    
+    wins = 0;
+    answered = 0;
+    secretWord = [];
+    toCompare = [];
+    mistakes = [];
+
+    var play = confirm("To play Bible trivia, read the description and press any key to guess the book in the Bible.");
+    if (play) {
+        updateScreen();
+    }
+}();
+/*
+Use key events to listen for the letters that your players will type.
+Display the following on the page:
+Press any key to get started!
+Wins: (# of times user guessed the word correctly).
+
+If the word is madonna, display it like this when the game starts: _ _ _ _ _ _ _.
+As the user guesses the correct letters, reveal them: m a d o _  _ a.
+
+Number of Guesses Remaining: (# of guesses remaining for the user).
+Letters Already Guessed: (Letters the user has guessed, displayed like L Z Y H).
+After the user wins/loses the game should automatically choose another word and make the user play it.
+
+Word Guess Game Bonuses
+
+Play a sound or song when the user guesses their word correctly, like in our demo.
+Write some stylish CSS rules to make a design that fits your game's theme.
+
+HARD MODE: Organize your game code as an object, except for the key events to get the letter guessed. This will be a challenge if you haven't coded with JavaScript before, but we encourage anyone already familiar with the language to try this out.
+Save your whole game and its properties in an object.
+Save any of your game's functions as methods, and call them underneath your object declaration using event listeners.
+Don't forget to place your global variables and functions above your object.
+
+Remember: global variables, then objects, then calls.
+
+Definitely talk with a TA or your instructor if you get tripped up during this challenge.
+
+A Few Tips
+
+IMPORTANT: Whichever assignment you choose, code your game one piece at a time! Code all of your apps one piece at a time. Always code one piece at a time!
+
+Pseudocode your program and break the app down into tiny, manageable fragments. This will make the coding process much less frustrating and a veritable Mach number faster. Otherwise, you'll be chipping away at a giant chunk of abstraction for way too many hours.
+
+The ability to solve a large problem by treating it as a set of smaller ones is the hallmark of a strong programmer. Best start adapting this into your development routine now, to better prepare for your more complex future projects.
+Remember:
+
+Split the whole program into many distinct, pseudocoded problems.
+Focus on one of the smaller problems and solve it.
+Only when you solve one problem should you then move onto your next problem.
+
+When you encounter bugs (and we all do), console.log will become your best friend. Regularly check your console to make sure your app is spitting out the right values.
+
+As a more advanced—but more powerful—alternative, feel free to experiment with the Chrome DevTools Debugger.
+
+Try your best to deliver a 'working/playable game' by the end of the deadline. If you're not making progress with Word Guess, switch gears to the Psychic game. Contact your TA/Instructor if you're not making progress after 2 hours. We're here to help!
+Substance over style! Submitting a working game matters more that making a broken app that at least looks pretty. We're focusing on game mechanics, not just on the look and feel of your app.
+That said, coding a functional app that also looks pretty would be impressive.
+Always commit your work and back it up with GitHub pushes. You don't want to lose hours of your work because you didn't push it to GitHub every half hour or so.
+
+Commit often.
+
+Turn in anything you have! Even if you don't finish, we still want to see what you were able to accomplish in the time we gave you. This will help us know what concepts we could help you with, as well as what topics we should focus on in the coming lectures.
+
+Reminder: Submission on BCS
+
+Please submit both the deployed Github.io link to your homework AND the link to the Github Repository!
+
+Minimum Requirements
+
+Attempt to complete homework assignment as described in instructions. If unable to complete certain portions, please pseudocode these portions to describe what remains to be completed. Adding a README.md as well as adding this homework to your portfolio are required as well and more information can be found below.
+
+Create a README.md
+
+Add a README.md to your repository describing the project. Here are some resources for creating your README.md. Here are some resources to help you along the way:
+
+About READMEs
+Mastering Markdown
+
+Add To Your Portfolio
+
+After completing the homework please add the piece to your portfolio. Make sure to add a link to your updated portfolio in the comments section of your homework so the TAs can easily ensure you completed this step when they are grading the assignment. To receive an 'A' on any assignment, you must link to it from your portfolio.
+
+One More Thing
+*/
